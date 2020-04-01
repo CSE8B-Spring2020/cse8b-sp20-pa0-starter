78c58a38982e6aaa39e9fbd99a7e24a178137a96
winny7 <45691074+winny7@users.noreply.github.com>
2020-04-01 13:46:29 -0700

    Update PA0.java

--- PA0.java
+++ PA0.java
@@ -1,7 +1,18 @@
 public class PA0 {
 
     public static void main(String[] args) {
+        
         // Test the printIntroduction method here
+        
+        printIntroduction("Annie");
+        printIntroduction("Winny Wilson");
+        
+        /* Test the printIntroduction method with many other test cases
+           For example: The input argument could be null, empty string, 
+                        string with multiple space characters, string 
+                        with 30 characters, and so on.
+        */
+        
     }
 
 
