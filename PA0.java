2508123440ea7efd4bb359c0e9210ca4616c5171
Adalbert Gerald Soosai Raj <gerald@eng.ucsd.edu>
2020-04-01 17:10:20 -0700

    Updated the comments style.

--- PA0.java
+++ PA0.java
@@ -1,23 +1,22 @@
 public class PA0 {
 
     public static void main(String[] args) {
-        
+
         // Test the printIntroduction method here
-        
+
         printIntroduction("Annie");
         printIntroduction("Winny Wilson");
-        
-        /* Test the printIntroduction method with many other test cases
-           For example: The input argument could be null, empty string, 
-                        string with multiple space characters, string 
-                        with 30 characters, and so on.
-        */
-        
+
+        // Test the printIntroduction method with many other test cases
+        // For example: The input argument could be null, empty string,
+        // string with multiple space characters, string with 30 characters,
+        // and so on.
+
     }
 
 
     public static void printIntroduction(String name) {
         // TODO: Fill in this method for PA0
     }
-    
+
 }
